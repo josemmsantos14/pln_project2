ed4f5b75196b8cc6976537bd417ba57cd1ef5be3
José Santos <josemmsantos14@gmail.com>
2023-05-30 18:02:11 +0100

    url_getter (2)

--- url_getter/en_url_getter.py
+++ url_getter/en_url_getter.py
@@ -1,39 +1,112 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 
-def extractDiseasePage(url):
-    page_html = requests.get(url).text
-    page_soup = BeautifulSoup(page_html, "html.parser")
+#region VariblesDeclar
 
-    page_div = page_soup.find("div", class_="field-name-body")
-    res = page_div.div.div
-    res.name = "page"
-    res.attrs = {}
-    return str(res)
+# URLS PRINCIPAIS
+url_mayo_principal = "https://www.mayoclinic.org"
+url_mayo_diseases = url_mayo_principal + "/diseases-conditions/"
 
-def extractDiseaseList(div):
-    desc = div.find("div", class_="field-content").text
-    title = div.div.h3.a.text
-    #print(div, end="\n\n")
-    return title, desc
+# LISTA DAS LISTAS DE TODAS AS DOENÇAS DE TODAS AS LETRAS
+ul_list_all_letters = []
+
+# LISTA DE LINKS DE CADA DOENÇA
+url_dic_disease = {}
+
+# DICIONARIO DAS INFOS DE CADA DOENÇA: SINTOMAS, CAUSAS...
+disease_info = {}
+disease_all_info = {}
+
+#endregion
+
+#region ProcessFirstPage
+
+# HTML DA PÁGINA PRINCIPAL A SER PROCESSADA
+html = requests.get(url_mayo_diseases).text
+html_first = BeautifulSoup(html,"html.parser")
+
+# BUSCA DAS ANCHOR TAGS DAS DOENÇAS POR LETRA INICIAL
+div_principal = html_first.find("div", class_="cmp-alphabet-facet cmp-button__inner--type-circle cmp-button__inner--color-primary-inverse")
+ul_principal = div_principal.ul
+#LISTA DE CADA LETRA
+list_items = ul_principal.findAll("li")
+
+# PERCORRER TODAS AS LETRAS ALFABETICAMENTE
+for li in list_items[:len(list_items)-1]:
+    anchor = li.div.a
+    anchor_href = anchor["href"]
+    # anchor_text = anchor.text
+
+    # HTML DA PÁGINA RESPETIVA DE CADA LETRA
+    diseases_by_letter = requests.get(url_mayo_principal + anchor_href).text
+    diseases_by_letter_soap = BeautifulSoup(diseases_by_letter, "html.parser")
+    # CONTAINER DAS DOENÇAS
+    diseases_container = diseases_by_letter_soap.find("div", class_="cmp-back-to-top-container__children")
+    # LISTA DAS SEGUNDAS LETRAS ALFABETICAMENTE ORGIGANIZADAS
+    ul_letter = diseases_container.findAll("ul")
+    for ul in ul_letter:
+        ul_list_all_letters.append(ul)
+
+#endregion
+
+#region ProcessSecondPage
+
+for ul_secondary in ul_list_all_letters:
+    li_disease = ul_secondary.find_all("li")
+    for li in li_disease:
+        anchor_disease = li.div.div.find("a", class_="cmp-result-name__link")
+        if anchor_disease:
+            disease = anchor_disease.text
+            disease_link = anchor_disease["href"]
+            url_dic_disease[disease] = disease_link
+
+#endregion
+
+#region ProcessThirdPage
 
 
-url_mayo = "https://www.mayoclinic.org/diseases-conditions/"
-search_select = "index?letter="
-html = requests.get(url_mayo).text
-
-# print("html:" + html)
-
-soup = BeautifulSoup(html,"html.parser")
-
-div_principal = soup.find("div", class_="cmp-alphabet-facet cmp-button__inner--type-circle cmp-button__inner--color-primary-inverse")
-ul = div_principal.ul
-list_items = ul.findAll("li")
-
-for li in list_items:
+for disease, url_disease in url_dic_disease.items():
+    text = ""
+    title = ""
+    disease_info.clear()
     
+    print(disease, " : ", url_disease, "\n")
+    
+    disease_page = BeautifulSoup(requests.get(url_disease).text,"html.parser")
+    time.sleep(1)
+    print(disease_page)
+    if disease_page is not None:
+        infos_container = disease_page.find("div", class_="content").find("div", id= "phmaincontent_0_ctl01_divByLine").find_next_sibling("div")
+        if infos_container is not None:
+            for tag in infos_container.children:
+                if tag.name == "h2" and tag.text != "":
+                    # PARA GUARDAR OS TITLE E TEXT PELO MEIO
+                    if title != "" and text != "":
+                        disease_info[title] = text
+                    title = tag.text
+                    text = ""
+                    
+                    # print("HEADER_2: ", tag, "\n")
+                # elif title != "" and tag.name == "h3" and tag.text != "":
+                #     semi_title = tag.text
+                #     text = ""
+                elif (tag.name == "p" or tag.name == "ul" or tag.name == "h3") and tag.text != "":
+                    text += str(tag)
 
+            # PARA GUARDAR OS TITLE E TEXT FINAL
+            disease_info[title] = text
+            # PARA GUARDAR TODAS AS INFORMAÇÕES RELATIVAS A CADA DOENÇA
+            disease_all_info[disease] = disease_info
+
+        else:
+            print("infos_container not found")
+    else:
+        print("disease_page not found")
+
+print(disease_all_info)
+#endregion 
 
 # urls = []
 # for div in divs:
@@ -55,7 +128,7 @@ for li in list_items:
 
         
 # print(lista)
-# file = open("Aula9/doencas.json","w", encoding="utf8")
+# file = open("./output/en_diseases.json","w", encoding="utf8")
 # json.dump(lista,file, ensure_ascii=False, indent = 4)
 # file.close()
 
